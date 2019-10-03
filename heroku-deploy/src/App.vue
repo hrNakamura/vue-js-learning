ab8915134c572bf2e2014cf79467456510ff3913
hiroshi.nh.nakamura <hiroshi.nh.nakamura@jp.ricoh.com>
2019-10-03 10:00:09 +0900

    add heroku-test project

---
+++ heroku-deploy/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld v-bind:msg="message"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  computed: {
+    message() {
+      const req = new XMLHttpRequest
+      let message = ''
+      req.open('GET', '/foo', false)
+      req.onload = function(){
+        message = JSON.parse(req.responseText).msg
+      }
+      req.send(null)
+      return message
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
